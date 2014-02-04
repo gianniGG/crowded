0722b6313ea20e0825f79ca347b454cadf72f0e6
TomShacham <shachamtom@gmail.com>
2014-02-04 16:21:40 +0000

    complete stripe integration

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => Rails.application.secrets.stripe_key,
+  :secret_key      => Rails.application.secrets.stripe_secret
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
