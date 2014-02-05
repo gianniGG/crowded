b0033ee253474ad20e838d7e3236da9e57e1990d
TomShacham <shachamtom@gmail.com>
2014-02-05 15:17:21 +0000

    breakout child view and parent

--- app/assets/javascripts/views/projects_index.js
+++ app/assets/javascripts/views/projects_index.js
@@ -1,6 +1,14 @@
 CrowddedApp.Views.ProjectsIndex = Backbone.View.extend({
   render: function() {
-    this.$el.html(JST['projects/index']({projects: this.collection}));
+    var self = this;
+
+    this.$el.html(JST['projects/index']());
+
+    this.collection.each(function(project){
+      var projectView = new CrowddedApp.Views.ProjectView({model:project});
+      self.$('div').append(projectView.render().el);
+    });
+
     return this;
   }
 });
