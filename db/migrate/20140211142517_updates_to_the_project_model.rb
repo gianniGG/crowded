19e81e3351d395157c0b7b4418a7611368a6e077
James Brooke <james@tnwo.co>
2014-02-11 16:04:58 +0000

    Tidied up attributes on the project model

---
+++ db/migrate/20140211142517_updates_to_the_project_model.rb
@@ -0,0 +1,13 @@
+class UpdatesToTheProjectModel < ActiveRecord::Migration
+  def change
+    rename_column :projects, :mission_statement, :headline
+    add_column :projects, :target, :integer
+    add_column :projects, :duration, :integer
+    add_column :projects, :project_info, :text
+    rename_column :projects, :funds, :funds_raised
+    add_column :projects, :hit_target, :boolean
+    add_column :projects, :approved, :boolean
+    add_column :projects, :company_editable, :boolean
+    remove_column :projects, :donors
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140211112546) do
+ActiveRecord::Schema.define(version: 20140211160256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,12 +74,16 @@ ActiveRecord::Schema.define(version: 20140211112546) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.string   "mission_statement"
-    t.string   "donors"
-    t.string   "creator"
+    t.string   "headline"
     t.string   "hashed_id"
     t.integer  "company_id"
-    t.integer  "funds"
+    t.integer  "funds_raised"
+    t.integer  "target"
+    t.integer  "duration"
+    t.text     "project_info"
+    t.boolean  "hit_target"
+    t.boolean  "approved"
+    t.boolean  "company_editable"
   end
 
   add_index "projects", ["company_id"], name: "index_projects_on_company_id", using: :btree
